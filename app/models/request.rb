9b57477b8b7f053e162b67f6449f04b0d6cccb50
Anton Kostin <schnibba@gmail.com>
2017-08-08 19:15:04 +0300

    Remarks from Adik.

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,5 @@
 class Request < ApplicationRecord
 
-  validates :first_name, :last_name, :company, :position, :email, :phone, presence: true
+  validates :first_name, :last_name, :company, :position, :email, :phone, :facebook, presence: true
 
 end
