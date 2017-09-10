ec51ed7eddda180bc9d16e68ff5618b3221d16f9
Anton Kostin <schnibba@gmail.com>
2017-09-10 23:33:02 +0300

    Front page rework, but no rails styling.

--- app/controllers/front/static_pages_controller.rb
+++ app/controllers/front/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class Front::StaticPagesController < FrontController
   def home
     @password = User.generate_password
+    @speakers = Speaker.all
+    @lectures = Event.last.lectures
+    @organizers = Event.last.organizers
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908152739) do
+ActiveRecord::Schema.define(version: 20170910203011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 20170908152739) do
     t.datetime "updated_at", null: false
     t.bigint "event_organizer_id"
     t.string "logo_grey"
+    t.string "url"
     t.index ["event_organizer_id"], name: "index_organizers_on_event_organizer_id"
   end
 
