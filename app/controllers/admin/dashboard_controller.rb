3ed06c7f827a67b4dd48479218dc28b8b48542f4
Anton Kostin <schnibba@gmail.com>
2017-09-26 17:59:13 +0300

    Change dashboard titles and order chart results asc.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -8,6 +8,7 @@ class Admin::DashboardController < AdminController
     # Chart data
     from = Time.zone.now.days_ago(14).beginning_of_day
     to   = Time.zone.now
-    @two_weeks_requests = Request.group('date(created_at)').where(created_at: from .. to).count.to_json
+    @two_weeks_requests = Request.where(created_at: from .. to).group('date(created_at)').order('date(created_at)').count.to_json
+    # debug
   end
 end
